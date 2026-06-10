f1c7d6df05df14d4486a6fd3e51640c5c6fbee8f
devlucasaf <freitas.lucasaf@gmail.com>
2026-06-10 17:29:19 -0300

    adiciona o mini game Football Números

---
+++ scripts/games/football-numeros/core.js
@@ -0,0 +1,9 @@
+export const estado = {
+    jogadores:              [],
+    sorteados:              [],
+    rodadaAtual:            0,
+    acertos:                0,
+    tentativasRestantes:    3,
+    totalRodadas:           10,
+    historico:              []
+};
