d918077817daba63f7a3362dce07aa611f71639a
devlucasaf <freitas.lucasaf@gmail.com>
2026-06-10 17:28:59 -0300

    adiciona o mini game Football Acerta Treinador

---
+++ scripts/games/football-acerta-treinador/core.js
@@ -0,0 +1,10 @@
+export const estado = {
+    treinadores:    [],
+    sorteados:      [],
+    rodadaAtual:    0,
+    pontos:         0,
+    totalRodadas:   10,
+    dicasReveladas: 0,
+    maxDicas:       5,
+    historico:      []
+};
