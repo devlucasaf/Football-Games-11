684321bd3e26eadf79523f48edb21fc575dcd063
devlucasaf <freitas.lucasaf@gmail.com>
2026-07-06 15:22:29 -0300

    remove o football quem falta

--- src/games/football-quem-falta/scripts/core.js
+++
@@ -1,9 +0,0 @@
-export const estado = {
-    desafios:           [],
-    sorteados:          [],
-    rodadaAtual:        0,
-    acertos:            0,
-    totalRodadas:       10,
-    tentativasRodada:   2,
-    usouDica:           false
-};
